7791e15a394d5c479c7adfbe3a07eef016d5bc83
byliu <bingyu513@yeah.net>
2018-06-24 23:26:04 +0800

    fixing bugs

--- job_ind.sh
+++ job_ind.sh
@@ -7,6 +7,11 @@
 # Permission to use and modify this script is granted.
 # I am not responsible for any errors in this script, so be forewarned!
 
+#$ -cwd
+###################$ -pe openmpi 17 # Moved to submit_job_ind
+#$ -S /bin/sh
+#$ -V
+
 # Modify these to put the stdout and stderr files in the right place for your system.
 ##$ -o ~/job-nobackup.$JOB_ID.$TASK_ID.out
 ##$ -e ~/job-nobackup.$JOB_ID.$TASK_ID.err
@@ -20,10 +25,8 @@ echo "Starting job: $n / ${config[@]}"
 
 # Call my codes. The last term is negative meaning the jobs are controlled by SGE
 matlab -nodisplay -nosplash -r\
+ "BATCH_GUI_Tempo_Analysis('$BatchPath','$BatchFile',0,0,-1,-1,-[$n,${config[@]}])"
 
-#"addpath(genpath('~/Z/Labtools'))"
-
-"BATCH_GUI_Tempo_Analysis('$BatchPath','$BatchFile',0,0,-1,-1,-[$n,${config[@]}])"
 #BATCH_GUI_Tempo_Analysis('/ion/gu_lab/byliu/Z/Data/TEMPO/BATCH/','test.m',0,0,-1,-1)
 
 echo "Done with job: $n"
