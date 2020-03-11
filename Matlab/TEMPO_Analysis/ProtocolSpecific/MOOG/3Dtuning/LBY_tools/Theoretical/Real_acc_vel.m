3bc52d46c9c73359c022512f1fee5747b1f24ba5
Eco Luo <bingyu513@yeah.net>
2020-03-11 17:30:14 +0800

    Correct  Real_acc_vel.m

--- Matlab/TEMPO_Analysis/ProtocolSpecific/MOOG/3Dtuning/LBY_tools/Theoretical/Real_acc_vel.m
+++ Matlab/TEMPO_Analysis/ProtocolSpecific/MOOG/3Dtuning/LBY_tools/Theoretical/Real_acc_vel.m
@@ -1,7 +1,7 @@
 % to plot real acceleration measured
 % LBY20180617
 
-function [time_vel,veloc,time_acc,accel] = Real_acc_vel(fig_flag)
+function Real_acc_vel(fig_flag)
 
 if ~nargin
     fig_flag = 1;
@@ -32,18 +32,46 @@ end
 % time_acc = TEMPO1_Acceleration.times(system_delay*2000+2:(system_delay+duration)*2000+2)-system_delay;
 % veloc = vel(system_delay*2000+2:(system_delay+duration)*2000+2);
 % accel = acc(system_delay*2000+2:(system_delay+duration)*2000+2);
-time_vel = TEMPO1_Acceleration.times(3:duration*2000+3);
-time_acc = TEMPO1_Acceleration.times(3:duration*2000+3);
-veloc = vel(3:duration*2000+3);
-accel = acc(3:duration*2000+3);
+time_vel = TEMPO1_Acceleration.times(3:duration*2000+2);
+time_acc = TEMPO1_Acceleration.times(3:duration*2000+2);
+veloc = vel(3:duration*2000+2);
+accel = acc(3:duration*2000+2);
+[~,vMaxT] = max(veloc); vMaxT = vMaxT/2
+[~,aMaxT] = max(accel); aMaxT = aMaxT/2
+[~,aMinT] = min(accel); aMinT = aMinT/2
+velSmooth = smooth(veloc,10);
+accSmooth = smooth(accel,150);
+[~,vMaxTSmooth] = max(velSmooth); vMaxTSmooth = vMaxTSmooth/2
+[~,aMaxTSmooth] = max(accSmooth); aMaxTSmooth = aMaxTSmooth/2
+[~,aMinTSmooth] = min(accSmooth); aMinTSmooth = aMinTSmooth/2
 
-figure(3);clf;
+
+% figure(3);clf;
+% if fig_flag == 0
+%     set(gcf,'visible','off');
+% end
+% set(gcf,'name','Gaussian Curve','pos',[200 200 1200 700]);
+% axes('pos',[0.2,0.2,0.6,0.6]);
+% [h,hLine1,hLine2] = plotyy(time_vel,veloc,time_acc,accel);
+% set(hLine1,'LineStyle','-','color','r','linewidth',4);
+% set(hLine2,'LineStyle','-','color',colorDBlue,'linewidth',4);
+% set(h(1),'yColor','r');
+% set(h(2),'yColor',colorDBlue);
+% ylabel(h(1),'Velocity (m/s) ');
+% ylabel(h(2),'Acceleration (m/s^2) ');
+% set(gca,'xlim',[0,1.5]);
+% xlabel('Time (s)');
+% title('Time profile(Real)');
+% SetFigure(25);
+
+% plot figures with smoothing
+figure(4);clf;
 if fig_flag == 0
     set(gcf,'visible','off');
 end
 set(gcf,'name','Gaussian Curve','pos',[200 200 1200 700]);
 axes('pos',[0.2,0.2,0.6,0.6]);
-[h,hLine1,hLine2] = plotyy(time_vel,veloc,time_acc,accel);
+[h,hLine1,hLine2] = plotyy(time_vel,velSmooth,time_acc,accSmooth);
 set(hLine1,'LineStyle','-','color','r','linewidth',4);
 set(hLine2,'LineStyle','-','color',colorDBlue,'linewidth',4);
 set(h(1),'yColor','r');
@@ -52,6 +80,7 @@ ylabel(h(1),'Velocity (m/s) ');
 ylabel(h(2),'Acceleration (m/s^2) ');
 set(gca,'xlim',[0,1.5]);
 xlabel('Time (s)');
-title('Time profile(Real)');
+title('Time profile(Real, with soothing)');
 SetFigure(25);
+
 end
